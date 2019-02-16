83d970a283b5d18b7b6d2d026647e42bc1e07a8e
Crow26 <7tacson7@gmail.com>
2019-02-17 00:34:56 +0300

    Подстановка из шаблона categories.php

--- templates/404.php
+++ templates/404.php
@@ -1,12 +1,4 @@
-<nav class="nav">
-    <ul class="nav__list container">
-        <?php foreach ($categories as $value): ?>
-        <li class="nav__item">
-            <a href="all-lots.html"><?=$value;?></a>
-        </li>
-        <?php endforeach; ?>
-    </ul>
-</nav>
+<?=$categories_list;?>
 <section class="lot-item container">
     <h2>404 Страница не найдена</h2>
     <p>Данной страницы не существует на сайте.</p>
