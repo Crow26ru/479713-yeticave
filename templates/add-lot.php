b0bbdfa8a5fe6123f64a17b22e50fbe84efc94c8
Семен <7tacson7@gmail.com>
2019-02-19 16:18:48 +0300

    Внесены правки в шаблон формы добавления нового лота

--- templates/add-lot.php
+++ templates/add-lot.php
@@ -1,13 +1,15 @@
 <?=$categories_list;?>
-<form class="form form--add-lot container form--invalid" action="https://echo.htmlacademy.ru" method="post"> <!-- form--invalid -->
+<form class="form form--add-lot container <?php if($errors): ?>form--invalid<?php endif;?>" action="add.php" method="post" enctype="multipart/form-data"> <!-- form--invalid -->
     <h2>Добавление лота</h2>
     <div class="form__container-two">
-        <div class="form__item form__item--invalid"> <!-- form__item--invalid -->
+        <div class="form__item <?php if(isset($errors['lot-name'])):?>form__item--invalid<?php endif; ?>"> <!-- form__item--invalid -->
           <label for="lot-name">Наименование</label>
           <input id="lot-name" type="text" name="lot-name" placeholder="Введите наименование лота" required>
-          <span class="form__error">Введите наименование лота</span>
+          <?php if(isset($errors['lot-name'])):?>
+          <span class="form__error"><?=$errors['lot-name'];?></span>
+          <?php endif; ?>
         </div>
-        <div class="form__item">
+        <div class="form__item <?php if(isset($errors['category'])):?>form__item--invalid<?php endif; ?>">
           <label for="category">Категория</label>
           <select id="category" name="category" required>
             <option>Выберите категорию</option>
@@ -15,15 +17,21 @@
               <option><?=$value;?></option>
             <?php endforeach; ?>
           </select>
-          <span class="form__error">Выберите категорию</span>
+          <?php if(isset($errors['category'])):?>
+          <span class="form__error"><?=$errors['category'];?></span>
+          <?php endif; ?>
         </div>
       </div>
-      <div class="form__item form__item--wide">
+      <div class="form__item form__item--wide <?php if(isset($errors['message'])):?>form__item--invalid<?php endif; ?>">
         <label for="message">Описание</label>
         <textarea id="message" name="message" placeholder="Напишите описание лота" required></textarea>
-        <span class="form__error">Напишите описание лота</span>
+        <?php if(isset($errors['message'])):?>
+        <span class="form__error"><?=$errors['message'];?></span>
+        <?php endif; ?>
       </div>
+
       <div class="form__item form__item--file"> <!-- form__item--uploaded -->
+
         <label>Изображение</label>
         <div class="preview">
           <button class="preview__remove" type="button">x</button>
@@ -31,28 +39,40 @@
             <img src="img/avatar.jpg" width="113" height="113" alt="Изображение лота">
           </div>
         </div>
+
         <div class="form__input-file">
-          <input class="visually-hidden" type="file" id="photo2" value="">
+          <input class="visually-hidden" type="file" id="photo2" name="image" value="">
           <label for="photo2">
             <span>+ Добавить</span>
           </label>
         </div>
+        <?php if(isset($errors['lot-date'])):?>
+        <span class="form__error"><?=$errors['image'];?></span>
+        <?php endif; ?>
+
       </div>
+
       <div class="form__container-three">
-        <div class="form__item form__item--small">
+        <div class="form__item form__item--small <?php if(isset($errors['lot-rate'])):?>form__item--invalid<?php endif; ?>">
           <label for="lot-rate">Начальная цена</label>
           <input id="lot-rate" type="number" name="lot-rate" placeholder="0" required>
-          <span class="form__error">Введите начальную цену</span>
+          <?php if(isset($errors['lot-rate'])):?>
+          <span class="form__error"><?=$errors['lot-rate'];?></span>
+          <?php endif; ?>
         </div>
-        <div class="form__item form__item--small">
+        <div class="form__item form__item--small <?php if(isset($errors['lot-step'])):?>form__item--invalid<?php endif; ?>">
           <label for="lot-step">Шаг ставки</label>
           <input id="lot-step" type="number" name="lot-step" placeholder="0" required>
-          <span class="form__error">Введите шаг ставки</span>
+          <?php if(isset($errors['lot-step'])):?>
+          <span class="form__error"><?=$errors['lot-step'];?></span>
+          <?php endif; ?>
         </div>
-        <div class="form__item">
+        <div class="form__item <?php if(isset($errors['lot-date'])):?>form__item--invalid<?php endif; ?>">
           <label for="lot-date">Дата окончания торгов</label>
           <input class="form__input-date" id="lot-date" type="date" name="lot-date" required>
-          <span class="form__error">Введите дату завершения торгов</span>
+          <?php if(isset($errors['lot-date'])):?>
+          <span class="form__error"><?=$errors['lot-date'];?></span>
+          <?php endif; ?>
         </div>
       </div>
       <span class="form__error form__error--bottom">Пожалуйста, исправьте ошибки в форме.</span>
