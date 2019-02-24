f4f1b78ffd4005c75822f7ae9e23fa08b3311a26
Crow26 <7tacson7@gmail.com>
2019-02-24 21:13:26 +0300

    Создан сценарий для входа пользователей

--- login.php
+++ login.php
@@ -5,21 +5,28 @@ define(
     'SELECT name AS categories FROM categories;'
 );
 
+// Запрос e-mail из таблицы users
+define(
+    'EMAIL_CHECK',
+    'SELECT email FROM users WHERE email = ?;'
+);
+
 // Запрос для аутентификации
 define(
     'USER_AUTH',
-    'SELECT email, password
+    'SELECT email, password, name
      FROM users
-     WHERE email = ? AND password = ?;'
+     WHERE email = ?;'
 );
 
 require_once('functions.php');
 require_once('connect.php');
 
 $categories = [];
-$is_auth = rand(0, 1);
-$user_name = 'Семён';
+$is_auth = 0;
+$user_name = '';
 $page_name = 'Вход - YetiCave';
+$errors = false;
 
 
 $rows_categories = mysqli_query($con, CATEGORIES_LIST);
@@ -30,15 +37,73 @@ foreach($rows_categories as $value) {
     array_push($categories, $value['categories']);
 }
 
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $login = $_POST;
+    /*
+      Ключи:
+      E-mail: ['email'];
+      Пароль: ['password']
+    */
+
+    // Массив сообщений об ошибках
+    $errors = [];
+
+    // Проверяем, что поля заполнены
+    foreach($login as $key => $value) {
+        if(empty($login[$key])) {
+            $errors[$key] = 'Это поле надо заполнить';
+        }
+    }
+
+    // Валидируем полученный e-mail
+    // Если на предыдущем шаге e-mail пуст, то пропускаем этот шаг
+    if(!isset($errors['email']) && !filter_var($login['email'], FILTER_VALIDATE_EMAIL)) {
+        $errors['email'] = 'Введен некорректный e-mail';
+    } else {
+        // Если не было ошибки валидации пароля, то:
+        // Сверим есть ли e-mail в БД
+        // Если есть, то сверим и пароль
+
+        $stmt = mysqli_prepare($con, EMAIL_CHECK);
+        mysqli_stmt_bind_param($stmt, 's', $login['email']);
+        mysqli_stmt_execute($stmt);
+        $is_email = mysqli_stmt_get_result($stmt);
+        $is_email = mysqli_fetch_row($is_email);
+
+        if(!$is_email) {
+            $errors['email'] = 'Проверьте введенный e-mail';
+        } else {
+            $stmt = mysqli_prepare($con, USER_AUTH);
+            mysqli_stmt_bind_param($stmt, 's', $login['email']);
+            mysqli_stmt_execute($stmt);
+            $result = mysqli_stmt_get_result($stmt);
+            $result = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+            $pass = $result[0]['password'];
+            $name = $result[0]['name'];
+
+            if(!password_verify($login['password'], $pass)) {
+                $errors['password'] = 'Вы ввели неверный пароль';
+            } else {
+                $_SESSION['user'] = $name;
+                header('Location: ./');
+            }
+        }
+    }
+}
+
 
 $categories_content = include_template('categories.php', ['categories' => $categories]);
-$login_content = include_template('login.php',  ['categories_list' => $categories_content]);
+$login_content = include_template('login.php',  [
+                                                  'categories_list' => $categories_content,
+                                                  'errors'          => $errors
+                                                ]);
 $page = include_template('layout.php', [
-                                                  'content'        => $login_content,
-                                                  'categories'     => $categories,
-                                                  'user_name'      => $user_name,
-                                                  'is_auth'        => $is_auth,
-                                                  'page_name'      => $page_name
+                                                  'content'         => $login_content,
+                                                  'categories'      => $categories,
+                                                  'user_name'       => $user_name,
+                                                  'is_auth'         => $is_auth,
+                                                  'page_name'       => $page_name
 ]);
 
 print($page);
