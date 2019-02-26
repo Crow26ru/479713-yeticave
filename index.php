0063bd7e252019cae0162662ac6c99f5d2f1fc5e
Семен <7tacson7@gmail.com>
2019-02-26 10:41:17 +0300

    Единообразные кавычки

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 
 // Константы SQL запросов
 define(
-    "CATEGORIES_LIST",
-    "SELECT name AS categories FROM categories;"
+    'CATEGORIES_LIST',
+    'SELECT name AS categories FROM categories;'
 );
 define(
-    "NEW_LOTS_LIST",
-    "SELECT
+    'NEW_LOTS_LIST',
+    'SELECT
         lots.id,
         lots.name,
         categories.name AS category,
@@ -18,15 +18,15 @@ define(
      JOIN categories ON lots.category_id = categories.id
      WHERE date_end > NOW()
      ORDER BY date_add DESC
-     LIMIT 9;"
+     LIMIT 9;'
 );
 
 $is_auth = 0;
 $categories = [];
-$page_name = "Главная - YetiCave";
+$page_name = 'Главная - YetiCave';
 
-require("functions.php");
-require("connect.php");
+require('functions.php');
+require('connect.php');
 
 if(isset($_SESSION['user'])) {
     $user_name = $_SESSION['user'];
@@ -35,8 +35,8 @@ if(isset($_SESSION['user'])) {
     $user_name = '';
 }
 // Выполнение запросов к БД
-if (!$con) {
-    print("Ошибка соединения: " . mysqli_connect_error());
+if(!$con) {
+    print('Ошибка соединения: ' . mysqli_connect_error());
 } else {
     $res_categories = mysqli_query($con, CATEGORIES_LIST);
     $rows_categories = mysqli_fetch_all($res_categories, MYSQLI_ASSOC);
@@ -54,16 +54,16 @@ $categories_length = count($categories);
 $products = $rows_lots;
 
 // Подключение шаблонов
-$page_content = include_template("index.php",   [
-                                                  "categories"    => $categories,
-                                                  "products"      => $products
+$page_content = include_template('index.php',   [
+                                                  'categories'    => $categories,
+                                                  'products'      => $products
                                                 ]);
-$layout_content = include_template("layout.php", [
-                                                   "content"    => $page_content,
-                                                   "user_name"  => $user_name,
-                                                   "is_auth"    => $is_auth,
-                                                   "page_name"  => $page_name,
-                                                   "categories" => $categories
+$layout_content = include_template('layout.php', [
+                                                   'content'    => $page_content,
+                                                   'user_name'  => $user_name,
+                                                   'is_auth'    => $is_auth,
+                                                   'page_name'  => $page_name,
+                                                   'categories' => $categories
                                                  ]);
 
 // Отправка сформированной разметки из шаблонов
