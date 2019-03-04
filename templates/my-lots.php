453c2f2b52db4f50a4d21c52b8a28e8ab6af6a0e
Семен <7tacson7@gmail.com>
2019-03-04 15:41:01 +0300

    Изменен шаблон со ставками пользователя

--- templates/my-lots.php
+++ templates/my-lots.php
@@ -3,7 +3,7 @@
     <h2>Мои ставки</h2>
     <table class="rates__list">
     <?php foreach($rates as $rate): ?>
-        <tr class="rates__item">
+        <tr class="rates__item <?=$rate['is_win'] ? 'rates__item--win' : '';?> <?=rate['is_end'] ? 'rates__item--end' : '';?>">
             <td class="rates__info">
                 <div class="rates__img">
                     <img src="<?=$rate['image'];?>" width="54" height="40" alt="<?=$rate['category'];?>">
@@ -14,13 +14,20 @@
                 <?=$rate['category'];?>
             </td>
             <td class="rates__timer">
-                <div class="timer timer--finishing"><?=get_time_of_end_lot($lot['time'], true);?></div>
+                <div class="timer <?=$rate['is_finishing'] ? 'timer--finishing' : '';?> <?=$rate['is_end'] ? 'timer--end' : '';?> <?=rate['is_win'] ? 'timer--win' : '';?>">
+                    <?php if(rate['is_win']): ?>
+                    Ставка выйграла
+                    <?php else if(rate['is_end']): ?>
+                    Торги окончены
+                    <?php else {$rate['time'];} ?>
+                    <?php endif; ?>
+                </div>
             </td>
             <td class="rates__price">
-                <?=show_price($rate['cost']);?>
+                <?=show_price($rate['rate']);?>
             </td>
             <td class="rates__time">
-            <?=show_user_frendly_time($rate['cost']);?>
+            <?=show_user_frendly_time($rate['date_add']);?>
             </td>
         </tr>
     <?php endforeach; ?>
